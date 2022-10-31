bd2f7928ada6882c675fe15be4450c0c933e4f97
adnan ahmad <ahmadadnan2235@gmail.com>
2022-10-31 16:14:59 -0700

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,19 +1,26 @@
-#ifndef _FILE_IO_
-#define _FILE_IO_
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/uio.h>
-#include <fcntl.h>
+#include <sys/stat.h>
+#include <elf.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(char *s);
-int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_file(const char *src, const char *dest);
-ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-#endif
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void validate_elf(unsigned char *e_ident);
+unsigned int lit_to_big_endian(unsigned int x);
+void print_entry(unsigned int e_type, unsigned char *e_ident);
+
+#endif /* MAIN */
